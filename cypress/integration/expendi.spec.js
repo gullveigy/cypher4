22b72193b7deee12decc275afe123dbee81c207c
YING YAN <1804094745@qq.com>
2018-12-10 21:41:06 +0800

    Publish expendi spec.js file to travis

--- cypress/integration/expendi.spec.js
+++ cypress/integration/expendi.spec.js
@@ -10,7 +10,7 @@ describe("Expendi page", () => {
     cy.get('button.btn1').click();
     cy.contains('Please Fill in the Form Correctly.').should('exist');
     cy.get(".nav-item").contains('Manage Expenditures' ).click();
-    //cy.get('tbody').find('tr').should('have.length', 2);
+    cy.get('tbody').find('tr').should('have.length', 1);
     cy.get(".nav-item").contains('Record Expenditure' ).click();
     cy.get('input.form__input1').type(12);
     cy.get('.error').contains('amount').should('not.exist');
@@ -29,7 +29,7 @@ describe("Expendi page", () => {
     cy.get('button.btn1').click();
     cy.contains('Thanks for your Record').should('exist');
     cy.get('a.btn2').click();
-    //cy.get('tbody').find('tr').should('have.length', 3);
+    cy.get('tbody').find('tr').should('have.length', 1);
     //cy.get('tbody').find('tr:nth-child(3)').find('td:nth-child(2)').should('contain','Others');
   });
 
